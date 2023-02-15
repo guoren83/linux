ab432ee10e96de8d5d8a237a966cd920f48bb4d2
Guo Ren <guoren@linux.alibaba.com>
2023-02-15 15:59:13 +0800

    riscv: s64ilp32: Introduce xlen_t for RV64ILP32 Linux kernel
    When s64ilp32 landed, we couldn't use CONFIG_64/32BIT to distingue XLEN
    data types. Because the xlen is 64, but the long & pointer is 32 for
    s64ilp32. So introduce a new data type - "xlen_t" and use __riscv_xlen
    instead of "ifdef CONFIG_64/32BIT" macro.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>

--- arch/riscv/include/asm/processor.h
+++ arch/riscv/include/asm/processor.h
@@ -99,7 +99,7 @@ struct thread_struct {
 	/* Callee-saved registers */
 	unsigned long ra;
 	unsigned long sp;	/* Kernel mode stack */
-	unsigned long s[12];	/* s[0]: frame pointer */
+	xlen_t     s[12];	/* s[0]: frame pointer */
 	struct __riscv_d_ext_state fstate;
 	unsigned long bad_cause;
 	unsigned long envcfg;
@@ -133,8 +133,8 @@ static inline void arch_thread_struct_whitelist(unsigned long *offset,
 	((struct pt_regs *)(task_stack_page(tsk) + THREAD_SIZE		\
 			    - ALIGN(sizeof(struct pt_regs), STACK_ALIGN)))
 
-#define KSTK_EIP(tsk)		(task_pt_regs(tsk)->epc)
-#define KSTK_ESP(tsk)		(task_pt_regs(tsk)->sp)
+#define KSTK_EIP(tsk)		(ulong)(task_pt_regs(tsk)->epc)
+#define KSTK_ESP(tsk)		(ulong)(task_pt_regs(tsk)->sp)
 
 
 /* Do necessary setup to start up a newly executed thread. */
