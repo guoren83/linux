398fe3486c3fc031eedb4521359d28bc3d2ba9d9
Guo Ren <guoren@linux.alibaba.com>
2023-07-20 09:58:27 -0400

    riscv: qspinlock: errata: Add ERRATA_THEAD_WRITE_ONCE fixup
    The early version of T-Head C9xx cores has a store merge buffer
    delay problem. The store merge buffer could improve the store queue
    performance by merging multi-store requests, but when there are not
    continued store requests, the prior single store request would be
    waiting in the store queue for a long time. That would cause
    significant problems for communication between multi-cores. This
    problem was found on sg2042 & th1520 platforms with the qspinlock
    lock torture test.
    
    So appending a fence w.o could immediately flush the store merge
    buffer and let other cores see the write result.
    
    This will apply the WRITE_ONCE errata to handle the non-standard
    behavior via appending a fence w.o instruction for WRITE_ONCE().
    
    Reviewed-by: Leonardo Bras <leobras@redhat.com>
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>

---
+++ arch/riscv/include/asm/rwonce.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __ASM_RWONCE_H
+#define __ASM_RWONCE_H
+
+#include <linux/compiler_types.h>
+#include <asm/alternative-macros.h>
+#include <asm/vendorid_list.h>
+
+#if defined(CONFIG_ERRATA_THEAD_WRITE_ONCE) && !defined(NO_ALTERNATIVE)
+#define write_once_flush()				\
+do {							\
+	asm volatile(ALTERNATIVE(			\
+		__nops(1),				\
+		"fence w, o\n\t",			\
+		THEAD_VENDOR_ID,			\
+		ERRATA_THEAD_WRITE_ONCE,		\
+		CONFIG_ERRATA_THEAD_WRITE_ONCE)		\
+		: : : "memory");			\
+} while (0)
+
+#define __WRITE_ONCE(x, val)				\
+do {							\
+	*(volatile typeof(x) *)&(x) = (val);		\
+	write_once_flush();				\
+} while (0)
+#endif
+
+#include <asm-generic/rwonce.h>
+
+#endif	/* __ASM_RWONCE_H */
