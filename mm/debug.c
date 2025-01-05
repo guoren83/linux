b0877bca4731ce3ca0bfb378526bcc98ea248637
Guo Ren <guoren@linux.alibaba.com>
2025-01-05 15:42:23 +0800

    mm: abi_rv64ilp32: Adapt vm_flags_t struct
    With ILP32 ABI on 64-bit systems, BITS_PER_LONG equals 32. Hence,
    change vm_flags_t type to u64 when CONFIG_64BIT=y and all related
    BIT macros need ULL appended.
    
    Update show_smap_vma_flags() in proc/task_mmu.c for u64 wide.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>

--- mm/debug.c
+++ mm/debug.c
@@ -181,7 +181,11 @@ void dump_vma(const struct vm_area_struct *vma)
 	pr_emerg("vma %px start %px end %px mm %px\n"
 		"prot %lx anon_vma %px vm_ops %px\n"
 		"pgoff %lx file %px private_data %px\n"
+#ifdef CONFIG_64BIT
+		"flags: %#llx(%pGv)\n",
+#else
 		"flags: %#lx(%pGv)\n",
+#endif
 		vma, (void *)vma->vm_start, (void *)vma->vm_end, vma->vm_mm,
 		(unsigned long)pgprot_val(vma->vm_page_prot),
 		vma->anon_vma, vma->vm_ops, vma->vm_pgoff,
