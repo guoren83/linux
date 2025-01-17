9e28ab4578de6737f799b9fa7574cc6a128bc4aa
Guo Ren <guoren@linux.alibaba.com>
2025-01-17 10:45:36 +0800

    iov_iter: abi_rv64ilp32: Make the size of kvec equal to iov_iter's
    As drivers/vhost/vringh.c needs BUILD_BUG_ON(sizeof(struct iovec)
    != sizeof(struct kvec)), make them equal.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>

--- include/linux/uio.h
+++ include/linux/uio.h
@@ -17,7 +17,13 @@ typedef unsigned int __bitwise iov_iter_extraction_t;
 
 struct kvec {
 	void *iov_base; /* and that should *never* hold a userland pointer */
+#if defined(CONFIG_64BIT) && (BITS_PER_LONG == 32)
+	u32  __iov_base;
+#endif
 	size_t iov_len;
+#if defined(CONFIG_64BIT) && (BITS_PER_LONG == 32)
+	u32  __iov_len;
+#endif
 };
 
 enum iter_type {
