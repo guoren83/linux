db19d012eb143a33efe4d20ee87be627b3225f44
Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>
2025-03-08 21:25:08 +0800

    rv64ilp32_abi: riscv: Separate modules from vmalloc range
    Divide modules into 64MiB range, independent of the vmalloc range.
    
     Virtual kernel memory layout:
           fixmap : 0x90a00000 - 0x90ffffff   (6144 kB)
           pci io : 0x91000000 - 0x91ffffff   (  16 MB)
          vmemmap : 0x92000000 - 0x93ffffff   (  32 MB)
          vmalloc : 0x94000000 - 0xb3ffffff   ( 512 MB)
          modules : 0xb4000000 - 0xb7ffffff   (  64 MB) <=
           lowmem : 0xc0000000 - 0xc7ffffff   ( 128 MB)
            kasan : 0x80000000 - 0x8fffffff   ( 256 MB)
           kernel : 0xb8000000 - 0xbfffffff   ( 128 MB)
    
    Signed-off-by: Guo Ren (Alibaba DAMO Academy) <guoren@kernel.org>

--- arch/riscv/include/asm/pgtable.h
+++ arch/riscv/include/asm/pgtable.h
@@ -48,7 +48,7 @@
 
 #define VMALLOC_SIZE     (KERN_VIRT_SIZE >> 1)
 #ifdef CONFIG_ABI_RV64ILP32
-#define VMALLOC_END      KERNEL_LINK_ADDR
+#define VMALLOC_END      MODULES_LOWEST_VADDR
 #else
 #define VMALLOC_END      PAGE_OFFSET
 #endif
@@ -69,9 +69,15 @@
 #define MODULES_VADDR		(PFN_ALIGN((unsigned long)&_end) - SZ_2G)
 #define MODULES_END		(PFN_ALIGN((unsigned long)&_start))
 #else
+#ifdef CONFIG_64BIT
+#define MODULES_LOWEST_VADDR	(KERNEL_LINK_ADDR - SZ_64M)
+#define MODULES_VADDR		MODULES_LOWEST_VADDR
+#define MODULES_END		KERNEL_LINK_ADDR
+#else
 #define MODULES_LOWEST_VADDR	VMALLOC_START
 #define MODULES_VADDR		VMALLOC_START
 #define MODULES_END		VMALLOC_END
+#endif
 #endif
 
 /*
